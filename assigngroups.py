37aaebc0c4744a24c77f78ec89406b0d6d91a6ac
bill <wckronholm@gmail.com>
2020-08-13 09:51:59 -0700

    PEP8; .gitignore

--- assigngroups.py
+++ assigngroups.py
@@ -24,10 +24,11 @@
 # SOFTWARE.
 
 from random import shuffle
-import subprocess # for printing
+import subprocess
 import argparse
 import datetime
 import sys
+
 
 def validdate(date):
     try:
@@ -36,73 +37,83 @@ def validdate(date):
     except ValueError:
         return False
 
+
 def sendtoprinter(filename):
     """
     Note: this probably only works on Linux machines.
     """
     with open(filename, 'r') as f:
-        lpr = subprocess.Popen("/usr/bin/lpr", stdin = subprocess.PIPE)
+        lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
         lpr.stdin.write(f.read())
     print("Go check the printer.")
     return
 
+
 def main():
     description = "Creates randomly assigned groups of students of similar sizes"
-    parser = argparse.ArgumentParser(prog='assigngroups',
-                                     description=description
-                                    )
-    parser.add_argument('-i', '--in-file',
-                        help='Name of the file containing the class roster',
-                        default='students.txt',
-                        dest='infile'
-                       )
-    parser.add_argument('-o', '--out-file',
-                        help='Name of the file to write the groups to',
-                        dest='outfile'
-                       )
-    parser.add_argument('-d', '--date',
-                        help='Date that the groups will be meeting in YYYYMMDD format'
-                       )
-    parser.add_argument('-p', '--print',
-                        action='store_true',
-                        dest='printit',
-                        help='Send the information to the default printer'
-                       )
-    parser.add_argument('-s', '--size',
-                        help='Size of the groups to aim for.\nDo not use with -n',
-                        type=int,
-                        dest='groupsize'
-                       )
-    parser.add_argument('-n', '--number-of-groups',
-                        help='Number of groups to make.\nDo not use with -s',
-                        type=int,
-                        dest='numberofgroups'
-                       )
-    parser.add_argument('-v', '--verbose',
-                        help='Lots of stuff gets printed out',
-                        action='store_true',
-                        dest='verbose'
-                       )
+    parser = argparse.ArgumentParser(
+        prog='assigngroups',
+        description=description
+    )
+    parser.add_argument(
+        '-i', '--in-file',
+        help='Name of the file containing the class roster',
+        default='students.txt',
+        dest='infile'
+    )
+    parser.add_argument(
+        '-o', '--out-file',
+        help='Name of the file to write the groups to',
+        dest='outfile'
+    )
+    parser.add_argument(
+        '-d', '--date',
+        help='Date that the groups will be meeting in YYYYMMDD format'
+    )
+    parser.add_argument(
+        '-p', '--print',
+        action='store_true',
+        dest='printit',
+        help='Send the information to the default printer'
+    )
+    parser.add_argument(
+        '-s', '--size',
+        help='Size of the groups to aim for.\nDo not use with -n',
+        type=int,
+        dest='groupsize'
+    )
+    parser.add_argument(
+        '-n', '--number-of-groups',
+        help='Number of groups to make.\nDo not use with -s',
+        type=int,
+        dest='numberofgroups'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        help='Lots of stuff gets printed out',
+        action='store_true',
+        dest='verbose'
+    )
     args = parser.parse_args()
-    
+
     if args.groupsize and args.numberofgroups:
         print("Don't give options for both -s and -n.\n")
         parser.print_help()
         sys.exit()
-    
+
     if args.verbose:
         verbose = True
     else:
         verbose = False
-    
+
     if args.infile:
         infile = args.infile
     else:
         infile = 'students.txt'
-    
+
     with open(infile, 'r') as f:
         students = f.readlines()
-    
+
     if args.date:
         date = args.date
         if not validdate(date):
@@ -111,28 +122,28 @@ def main():
             sys.exit()
     else:
         date = datetime.date.today().strftime("%Y%m%d")
-    
+
     if args.outfile:
         outfile = args.outfile
     else:
         outfile = date+'.txt'
-    
+
     if args.groupsize:
         groupsize = args.groupsize
     else:
         groupsize = 3
-    
+
     if args.numberofgroups:
         numberofgroups = args.numberofgroups
     else:    
         numberofstudents = len(students)
         numberofgroups = numberofstudents//groupsize
-    
+
     # fix in case groupsize > numberofgroups
     if numberofgroups == 0:
         numberofgroups = 1
-    
-    groups = {n:[] for n in range(numberofgroups)}
+
+    groups = {n: [] for n in range(numberofgroups)}
 
     # randomize the list
     shuffle(students)
@@ -142,29 +153,34 @@ def main():
         print("Dividing the students into %d groups of roughly %d size each" % (numberofgroups, groupsize))
         print("Saving groups to %s" % outfile)
         print("")
-        
+
     n = 0
     # separate into groups
     while len(students) > 0:
         groups[n].append(students.pop(0))
         n = (n+1) % numberofgroups
-    
+
     with open(outfile, 'w') as f:
         for key, value in groups.items():
-            if verbose: print("Group", key)
+            if verbose:
+                print("Group", key)
             f.write("Group "+str(key)+"\n")
             for x in value:
                 f.write(x)
-                if verbose: print(x.strip())
+                if verbose:
+                    print(x.strip())
             f.write("\n")
-            if verbose: print("")
-    
+            if verbose:
+                print("")
+
     if args.printit:
-        if verbose: print("Sending to the printer...")
+        if verbose:
+            print("Sending to the printer...")
         sendtoprinter(outfile)
-    
+
     print("Saved groups to %s" % outfile)
-    
+
+
 if __name__ == '__main__':
     main()
     sys.exit()
